3040d8420579895ea86d463e13ab9ab64e4eecc6
Kacper Stodulski <kacpers952@gmail.com>
2025-01-24 22:46:26 +0000

    Project API Registration done

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,15 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +17,35 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+
+
+@RestController
 public class SocialMediaController {
+
+    private AccountService accountService;
+    private MessageService messageService;
+
+    @Autowired
+    public SocialMediaController(AccountService accountService, MessageService messageService){
+        this.accountService = accountService;
+        this.messageService = messageService;
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<String> register(@RequestBody Account account){
+
+        if(account.getUsername().isBlank() || (account.getPassword()).length() < 3){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid username or password");
+        }
+        if(accountService.accountNameTaken(account.getUsername())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already taken.");
+        }
+        accountService.register(account);
+        return ResponseEntity.status(HttpStatus.OK).body("Sucessfully registered");
+    }
+    // @PostMapping("login")
+    // public ResponseEntity<Void> login(@RequestBody Account account){
+
+    // }
 
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,38 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public Account register(Account account) {
+        
+        return accountRepository.save(account);
+
+    }
+
+    public boolean accountNameTaken(String accountName){
+        List<Account> allAccounts = accountRepository.findAll();
+        
+        for(Account all: allAccounts){
+            if(all.getUsername().equals(accountName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
